5fbfd83be61a7db5fc24ec121798346ca8727563
kidusss <kidussolomon2004@gmail.com>
2020-05-24 14:34:44 +0300

    code cleanup

--- controllers/alerting.js
+++ controllers/alerting.js
@@ -1,11 +1,8 @@
 const { Telegram } = require("../notifications");
 
 const setWebHook = async (req, res) => {
-  await Telegram.send("394090585", " hello man");
-  console.log("the price is");
-  console.log(req.body);
   Telegram.webHookHandler(req.body);
-  res.status(200).send({ message: "web hook message" });
+  res.status(200).send({ message: "webhook message" });
 };
 
 module.exports = {
